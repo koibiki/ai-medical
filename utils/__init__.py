a9745497ed1e080c9d91a4899cd0c05bf7e53ecd
koibiki <orient2008.cl@hotmail.com>
2018-01-17 00:01:20 +0800

    create samples.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,11 @@
+import random
+import pandas as pd
+
+
 def create_sample(data):
-    columns = data.columns
-    length = data.shape[0]
-    count = 0
-    for item in data:
-        data
+    randoms = []
+    for index in range(len(data)):
+        random_value = [item * (0.95 + 0.1 * random.random()) for item in data.iloc[index].values]
+        random_series = pd.Series(random_value, index=data.columns)
+        randoms.append(random_series)
+    return pd.DataFrame(randoms)
