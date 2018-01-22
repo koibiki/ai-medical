07035de0a5a23291e6ff61df7d7e6cbe9ca5c2ec
chengli <lcheng.ext@orange.com>
2018-01-22 18:15:44 +0800

    filter error data.

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import cdist
 from sklearn.cluster import KMeans
+from dateutil.parser import parse
 
 from feature_engineering.segment_raw_data import segment_raw_data
 from feature_engineering.rank_feature import rank_feature, rank_feature_by_max, rank_feature_count
@@ -12,7 +13,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error
 from model_selection.cv import k_fold_regressor, balance_k_fold_regressor
-from utils import create_scale_feature, normalize_data_frame
+from utils import create_scale_feature, normalize_data_frame, delete_error_data, filtration
 
 # 分别求出高血糖 中血糖 正常血糖每个feature 与其中位值的差值
 # 将 血糖大于7的部分与 其他进行平衡抽样训练
@@ -29,46 +30,26 @@ test_data = test.iloc[:, 1:]
 
 train_data['性别'] = train_data['性别'].map({'男': 1, '女': 0})
 test_data['性别'] = test_data['性别'].map({'男': 1, '女': 0})
-# train_data = train_data.assign(delta胆固醇=train_data['总胆固醇'] - train_data['高密度脂蛋白胆固醇'] - train_data['低密度脂蛋白胆固醇'])
-# test_data = test_data.assign(delta胆固醇=test_data['总胆固醇'] - test_data['高密度脂蛋白胆固醇'] - test_data['低密度脂蛋白胆固醇'])
 
-# train_data = train_data.assign(血小板总体积=train_data['血小板计数'] * train_data['血小板平均体积'])
-# test_data = test_data.assign(血小板总体积=train_data['血小板计数'] * test_data['血小板平均体积'])
-
-# train_data = train_data.assign(红细胞总体积=train_data['红细胞计数'] * train_data['红细胞平均体积'])
-# test_data = test_data.assign(红细胞总体积=test_data['红细胞计数'] * test_data['红细胞平均体积'])
-
-# train_data = train_data.assign(白蛋白比例=train_data['白蛋白'] / train_data['*总蛋白'])
-# test_data = test_data.assign(白蛋白比例=test_data['白蛋白'] / test_data['*总蛋白'])
-
-# train_data = train_data.assign(球蛋白比例=train_data['*球蛋白'] / train_data['*总蛋白'])
-# test_data = test_data.assign(球蛋白比例=test_data['*球蛋白'] / test_data['*总蛋白'])
-
-# train_data = train_data.assign(红白比例=train_data['红细胞计数'] / train_data['血小板计数'])
-# test_data = test_data.assign(红白比例=test_data['红细胞计数'] / test_data['血小板计数'])
-
-# train_data = train_data.assign(红血比例=train_data['红细胞计数'] / train_data['血小板计数'])
-# test_data = test_data.assign(红血比例=test_data['红细胞计数'] / test_data['血小板计数'])
-
-# train_data = train_data.assign(白血比例=train_data['白细胞计数'] / train_data['血小板计数'])
-# test_data = test_data.assign(白血比例=test_data['白细胞计数'] / test_data['血小板计数'])
-
-train_data = train_data.drop(['体检日期'], axis=1)
-test_data = test_data.drop(['体检日期'], axis=1)
+train_data['体检日期'] = (pd.to_datetime(train_data['体检日期']) - parse('2016-10-09')).dt.days
+test_data['体检日期'] = (pd.to_datetime(test_data['体检日期']) - parse('2016-10-09')).dt.days
 
 columns = train_data.columns
 print(columns)
-str_columns = ['sex', 'age'] + ['f' + str(p) for p in range(len(columns)-2)]
+str_columns = ['sex', 'age', 'date'] + ['f' + str(p) for p in range(len(columns)-3)]
 
 train_data.columns = str_columns
 test_data.columns = str_columns
 train_target.name = 'Y'
 
-scale_train_data = train_data
+train_data_target = pd.concat([train_data, train_target], axis=1)
 
-describe = scale_train_data.describe()
-describe.median()
 
+train_data_target = delete_error_data(train_data_target)
+train_data_target = filtration(train_data_target)
+
+train_data = train_data_target.iloc[:, 1:-1]
+train_target = train_data_target.iloc[:, -1]
 train_data, factors = normalize_data_frame(train_data, start_index=2)
 train_data.fillna(-99, inplace=True)
 
@@ -79,28 +60,29 @@ scale_train_data = train_data
 # new_feature1 = segment_raw_data(scale_train_data.iloc[:, 2], 2)
 # scale_train_data = pd.concat([new_feature1, scale_train_data], axis=1)
 
-print(scale_train_data.shape)
 # cmf = ClassifierModelFactory()
-scale_train_target = pd.concat([scale_train_data, train_target], axis=1)
+# scale_train_target = pd.concat([scale_train_data, train_target], axis=1)
 
 
 rmf = RegressorModelFactory()
 
-X_train, X_valid, y_train, y_valid = train_test_split(scale_train_data, train_target, test_size=0.1, random_state=33)
+X_train, X_valid, y_train, y_valid = \
+    train_test_split(train_data, train_target, test_size=0.1, random_state=33)
 
-lgb_y_valid, kf_lgb_mse, cv_indexs = \
+lgb_y_valid, kf_lgb_mse, cv_lgm_indexs = \
     k_fold_regressor(X_train, y_train, X_valid, RegressorModelFactory.MODEL_LIGHET_GBM, cv=5)
-# xgb_y_valid, kf_xgb_mse = k_fold_regressor(X_train, y_train, X_valid, RegressorModelFactory.MODEL_XGBOOST, cv=5)
+xgb_y_valid, kf_xgb_mse, cv_xgb_indexs = \
+    k_fold_regressor(X_train, y_train, X_valid, RegressorModelFactory.MODEL_XGBOOST, cv=5)
 
 # model = rmf.create_model(RegressorModelFactory.MODEL_XGBOOST)
 # model = cmf.create_model(ClassifierModelFactory.MODEL_XGBOOST)
 # model.fit(X_train, X_valid, y_train, y_valid)
 # predict = model.predict(X_valid)
 # print(predict)
-y_pred = (lgb_y_valid )
+y_pred = (lgb_y_valid + xgb_y_valid)/2
 
 #
-print('kf mse:', (kf_lgb_mse )/2)
+print('kf mse:', (kf_lgb_mse + kf_xgb_mse)/4)
 print('mse : ', mean_squared_error(y_valid, y_pred)/2)
 
 # pd_pred = pd.DataFrame(y_pred, columns=['血糖'])
--- utils/__init__.py
+++ utils/__init__.py
@@ -20,7 +20,7 @@ def create_scale_feature(data):
         if index == 0:
             continue
         for j in range(index + 1, len(columns)):
-            new_data.insert(new_data.shape[1], columns[j] + '_' + columns[index], data.iloc[:, j]/data.iloc[:, index])
+            new_data.insert(new_data.shape[1], columns[j] + '_' + columns[index], data.iloc[:, j] / data.iloc[:, index])
     return new_data
 
 
@@ -59,7 +59,7 @@ def fix_min_all(data):
 
 def normalize_feature(data, f_min=0, f_max=100):
     d_min, d_max = min(data), max(data)
-    factor = (f_max - f_min)/(d_max - d_min)
+    factor = (f_max - f_min) / (d_max - d_min)
     normalized = f_min + (data - d_min) * factor
     return normalized, factor
 
@@ -78,8 +78,106 @@ def get_euclidean_metric(vec1, vec2):
 
 def get_cosine(vec1, vec2):
     np_vec1, np_vec2 = np.array(vec1), np.array(vec2)
-    return np_vec1.dot(np_vec2)/(math.sqrt((np_vec1 ** 2).sum()) * math.sqrt((np_vec2 ** 2).sum()))
+    return np_vec1.dot(np_vec2) / (math.sqrt((np_vec1 ** 2).sum()) * math.sqrt((np_vec2 ** 2).sum()))
 
 
 def get_cosine_angle(vec1, vec2):
     return math.acos(get_cosine(vec1, vec2)) / math.pi * 180
+
+
+def combine_all(arr, start, result, index, group_array):
+    for ct in range(start, len(arr) - index + 1):
+        result[index - 1] = ct
+        if index - 1 == 0:
+            copy = result.copy()
+            copy.reverse()
+            group_array.append(copy)
+        else:
+            combine_all(arr, ct + 1, result, index - 1, group_array)
+
+
+# 删除异常点
+def delete_error_data(df):
+    print(df.shape)
+    df = df[(df['f0'] < 200).values | np.isnan(df['f0'])]
+    print(df.shape)
+    df = df[(df['f1'] < 250).values | np.isnan(df['f1'])]
+    print(df.shape)
+    df['f2'] = df['f2'].apply(lambda x: x if x < 200 else 200)
+    print(df.shape)
+    df = df[(df['f3'] < 400).values | np.isnan(df['f3'])]
+    df['f3'] = df['f3'].apply(lambda x: x if x < 300 else 300)
+    print(df.shape)
+    df = df[(df['f4'] < 95).values & (df['f4'] > 60).values | np.isnan(df['f4'])]
+    print(df.shape)
+    df = df[(df['f6'] > 20).values & (df['f6'] < 50).values | np.isnan(df['f6'])]
+    print(df.shape)
+    df = df[(df['f7'] < 3).values | np.isnan(df['f7'])]
+    print(df.shape)
+    df = df[(df['f8'] < 20).values | np.isnan(df['f8'])]
+    print(df.shape)
+    df = df[(df['f9'] < 12.5).values | np.isnan(df['f9'])]
+    print(df.shape)
+    df = df[(df['f10'] < 3).values | np.isnan(df['f10'])]
+    print(df.shape)
+    df = df[(df['f17'] < 2.5).values | np.isnan(df['f17'])]
+    print(df.shape)
+    # 删除 f19 kf mse 0.97995  predict mse 0.7308
+    # 不删除   kf mse 0.95059  predict mse 1.0235
+    # df = df[(df['f19'] < 12.5).values | np.isnan(df['f19'])]
+    print(df.shape)
+    df = df[(df['f20'] < 16).values | np.isnan(df['f20'])]
+    print(df.shape)
+    df = df[(df['f28'] < 600).values | np.isnan(df['f28'])]
+    print(df.shape)
+    df = df[(df['f32'] > 25).values | np.isnan(df['f32'])]
+    print(df.shape)
+    df = df[(df['f33'] < 60).values | np.isnan(df['f33'])]
+    print(df.shape)
+    return df
+
+
+def filtration(df):
+    df['sex'] = df['sex'].apply(lambda x: 1 if np.isnan(x) else x)
+    df['f0'] = df['f0'].apply(lambda x: x if (x < 150) | np.isnan(x) else 150)
+
+    df['f1'] = df['f1'].apply(lambda x: x if (x < 200) | np.isnan(x) else 200)
+
+    df['f2'] = df['f2'].apply(lambda x: x if (x < 200) | np.isnan(x) else 200)
+
+    df['f3'] = df['f3'].apply(lambda x: x if (x < 300) | np.isnan(x) else 300)
+
+    df['f4'] = df['f4'].apply(lambda x: x if (x > 60) | np.isnan(x) else 60)
+    df['f4'] = df['f4'].apply(lambda x: x if (x < 95) | np.isnan(x) else 95)
+
+    df['f5'] = df['f5'].apply(lambda x: x if (x > 35) | np.isnan(x) else 35)
+    df['f5'] = df['f5'].apply(lambda x: x if (x < 55) | np.isnan(x) else 55)
+
+    df['f6'] = df['f6'].apply(lambda x: x if (x > 20) | np.isnan(x) else 20)
+    df['f6'] = df['f6'].apply(lambda x: x if (x < 50) | np.isnan(x) else 50)
+
+    df['f7'] = df['f7'].apply(lambda x: x if (x > 0.75) | np.isnan(x) else 0.75)
+    df['f7'] = df['f7'].apply(lambda x: x if (x < 2.5) | np.isnan(x) else 2.5)
+
+    df['f8'] = df['f8'].apply(lambda x: x if (x < 20) | np.isnan(x) else 20)
+
+    df['f9'] = df['f9'].apply(lambda x: x if (x < 12.5) | np.isnan(x) else 12.5)
+
+    df['f10'] = df['f10'].apply(lambda x: x if (x < 3) | np.isnan(x) else 3)
+
+    df['f13'] = df['f13'].apply(lambda x: x if (x < 140) | np.isnan(x) else 140)
+
+    df['f17'] = df['f17'].apply(lambda x: x if (x < 2.5) | np.isnan(x) else 2.5)
+
+    # df['f19'] = df['f19'].apply(lambda x: x if (x < 12.5) | np.isnan(x) else 12.5)
+
+    df['f20'] = df['f20'].apply(lambda x: x if (x < 16) | np.isnan(x) else 16)
+
+    df['f27'] = df['f27'].apply(lambda x: x if (x < 22) | np.isnan(x) else 22)
+
+    df['f28'] = df['f28'].apply(lambda x: x if (x < 500) | np.isnan(x) else 500)
+
+    df['f32'] = df['f32'].apply(lambda x: x if (x > 30) | np.isnan(x) else 30)
+
+    df['f33'] = df['f33'].apply(lambda x: x if (x < 60) | np.isnan(x) else 60)
+    return df
